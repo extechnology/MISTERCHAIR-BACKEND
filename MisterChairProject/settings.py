b3a2f16942b39cd5a8dfacd20055651d47a72fc4
Mridhul2k03 <mridhulkrishna11@gmail.com>
2026-02-11 15:00:45 +0530

    updates

--- MisterChairProject/settings.py
+++ MisterChairProject/settings.py
@@ -191,10 +191,6 @@ REST_FRAMEWORK = {
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
 
-    'DEFAULT_FILTER_BACKENDS': (
-        'django_filters.rest_framework.DjangoFilterBackend',
-    ),
-
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.CursorPagination',
     'PAGE_SIZE': 10,
 }
